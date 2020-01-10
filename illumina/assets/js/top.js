00f418869ef6a43a45697bf3e29b67455d9ecce4
Rebecca Young <rebeccayoung@Rebeccas-MacBook-Pro.local>
2020-01-09 22:42:54 -0600

    Further video enhancements

--- illumina/assets/js/top.js
+++ illumina/assets/js/top.js
@@ -41,17 +41,14 @@ $(function() {
   const $inviewEl = $(".inviewEl"),
     $window = $(window),
     $body = $("body"),
-    $campaignSection = $("#campaign"),
-    $campaignTagList = $("#campaign-tag-list");
+    $loadMoreBtn = $("#appendBtn");
   let winW, shadeModalID, $shadeModal, tabID;
 
   $inviewEl.on("inview", function() {
     $(this).addClass("inviewed");
   });
 
-  $campaignTagList.find("a").on("click", function() {
-    tabID = $(this).data("tab-id");
-    $campaignSection.find(".bolt-ver:not(#"+tabID+")").removeClass("current-list");
-    $campaignSection.find("#"+tabID).addClass("current-list");
-  });
+  console.log('load more button', $loadMoreBtn);
+
+  $loadMoreBtn.addClass('kurokami-btn--outline');
 });
