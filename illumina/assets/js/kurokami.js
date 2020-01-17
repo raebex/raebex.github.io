98619d728204ad770e398746bfbe2ae725f706bd
Rebecca Young <rebeccayoung@Rebeccas-MacBook-Pro.local>
2020-01-16 20:38:31 -0600

    add swipe

--- illumina/assets/js/kurokami.js
+++ illumina/assets/js/kurokami.js
@@ -37,6 +37,52 @@ function onPlayerReady(event){
   });
 }
 
+function swipedetect(el, callback){
+  var touchsurface = el,
+  swipedir,
+  startX,
+  startY,
+  distX,
+  distY,
+  threshold = 150, //required min distance traveled to be considered swipe
+  restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 300, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime,
+  handleswipe = callback || function(swipedir){}
+
+  touchsurface.addEventListener('touchstart', function(e){
+      var touchobj = e.changedTouches[0]
+      swipedir = 'none'
+      dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime() // record time when finger first makes contact with surface
+      e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchmove', function(e){
+      e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+
+  touchsurface.addEventListener('touchend', function(e){
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (elapsedTime <= allowedTime){ // first condition for awipe met
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+              swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+          }
+      }
+      handleswipe(swipedir)
+      e.preventDefault()
+  }, false)
+}
+
 $(function() {
   const $inviewEl = $(".inviewEl"),
     $window = $(window),
@@ -64,13 +110,48 @@ $(function() {
       $this.removeClass('reveal-after');
       $beforeIndicator.addClass('active');
       $afterIndicator.removeClass('active');
-      
     } else {
       $this.addClass('reveal-after');
       $beforeIndicator.removeClass('active');
       $afterIndicator.addClass('active');
     }
   });
+
+  if (mediaQueryMobile || mediaQueryTablet) {
+    const el = document.querySelector('#beforeafter--video .beforeafter__inner');
+    const beforeIndicator = el.parentNode.querySelector('.indicator--before');
+    const afterIndicator = el.parentNode.querySelector('.indicator--after');
+    const bobs = document.querySelectorAll('#bob .beforeafter__inner');
+
+    swipedetect(el, function(swipedir){
+        if (swipedir === 'left') {
+          el.parentNode.classList.add('reveal-after');
+          beforeIndicator.classList.remove('active');
+          afterIndicator.classList.add('active');
+        } else if (swipedir === 'right') {
+          el.parentNode.classList.remove('reveal-after');
+          beforeIndicator.classList.add('active');
+          afterIndicator.classList.remove('active');
+        }
+    });
+
+    bobs.forEach((bob) => {
+      let beforeIndicator = bob.parentNode.querySelector('.indicator--before');
+      let afterIndicator = bob.parentNode.querySelector('.indicator--after');
+
+      swipedetect(bob, function(swipedir){
+        if (swipedir === 'left') {
+          bob.parentNode.classList.add('reveal-after');
+          beforeIndicator.classList.remove('active');
+          afterIndicator.classList.add('active');
+        } else if (swipedir === 'right') {
+          bob.parentNode.classList.remove('reveal-after');
+          beforeIndicator.classList.add('active');
+          afterIndicator.classList.remove('active');
+        }
+      });
+    });
+  }
 
   loadVideo();
 
